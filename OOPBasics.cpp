0901f35f52bcab2ee266aa1b8796e1ce699b76c7
Shubham <shubham.pandey.191@cranfield.ac.uk>
2023-10-04 17:49:35 +0530

    OOPs basics

---
+++ OOPBasics.cpp
@@ -0,0 +1,25 @@
+//OOPs is just an approach.
+//as size increases readability maintainability bug-free nature decreases
+// works on concept of classes and objects.
+// A clas is a template to create objects
+//Unless objects are created there is no memory allocated to class
+// treats data as a critical for example bank balance
+// decomposes the problem in objects and builds data and functions around the objects
+
+/*
+Classes - Basic template for creating objects
+Objects - Basic run time entities
+Data Abstraction(separation of functionality) & Encapsulation - Wrapping data and functions into a single unit. Basically creating a class
+Inheritance - Properties of one class can be inherited into others
+Polymorphism - ability to take more than one forms
+Dynamic Binding - code which will execute is not known until the program runs.
+Message Passing - Object.message(Information) call format.
+*/
+
+/*
+Benefits:
+1. Better code reusability using objects and inheritance
+2. Principle of data hiding helps build secure systems.
+3. Multiple objects can co-exit without any interefence.
+4. Software complexity can be easily managed.
+*/
