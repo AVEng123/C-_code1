7238282ece8d09905d047e030edf335b2cdd5f11
Shubham <shubham.pandey.191@cranfield.ac.uk>
2023-10-05 13:12:42 +0530

    Constructor with Default Arguments

---
+++ ConstructorsDefaultArguments.cpp
@@ -0,0 +1,32 @@
+/*
+Constructor with Default Arguments
+*/
+
+#include<iostream>
+using namespace std;
+
+class Simple{
+    private:
+        int data1, data2;
+    public:
+        Simple(int a, int b = 9){
+            data1 = a;
+            data2 =b;   
+        }
+
+        void printData();
+};
+
+void Simple ::printData() {
+    cout << "The value of data is: " << data1 << " and " << data2 << endl;
+}
+
+int main(){
+    
+    Simple s(1);
+    s.printData();
+
+    Simple s1(2,7);
+    s1.printData();
+    return 0;
+}
